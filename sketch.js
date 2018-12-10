01978fa9ccbf39862ac17dd89d72ad40c29dbbcb
kyoheria <ishir456@newschool.edu>
2018-12-10 15:17:26 -0500

    initial commit

---
+++ sketch.js
@@ -0,0 +1,239 @@
+var positive =[];
+var posQuote =["The magical girl must to work to balance \n her normal life domestic obligations, educational obligations, \n and interpersonal relationships with her duty to protect the world.", 
+"I'm really excited about Haruka and Michiru's relationship, \n and keeping it true to the original. \n I think we're living in a really exciting time where \n that won’t be seen as scandalous. \n I think it’ll just be seen as a beautiful romance.",
+"Reading the manga version of Sailor Moon when I was in college \n helped me come to terms with my own bisexuality and \n realize that I wanted a relationship with another girl like Haruka and Michiru had.",
+"Speaking of that, Madoka's mother Junko is portrayed positively \n as a career woman with a stay-at-home husband who takes care of the baby. \n This is in contrast with career women in other anime, \n who are often seen as greedy, narcissistic villain \n types (like in Princess Jellyfish). Career women are also sometimes \n portrayed in anime as sluts (like Misato \n in Neon Genesis Evangelion). Puella Magi Madoka Magica in contrast \n seems to be speaking out about this trend by showing Junko as a positive \n (although somewhat imperfect) career woman.",
+"The Japanese anime series about teen girls named Sailor Senshi \n fighting bad guys from outer space, \n was a hallmark of girls’ after-school cartoons. \n It was many kids’ gateway to anime but, more importantly, \n Sailor Moon proved that children’s programming that \n centered on empowered young women had serious commercial \n power and popular appeal.",
+"Usagi (the titular Sailor Moon) often cries and acts like a damsel-in-distress, needing the mysterious man Tuxedo Mask to save her. In most versions of the story, this is fixed by character-development: Usagi’s “weakness” at first is simply due to her being a young teen who’s in over her head, and she doesn’t stay that way. As she grows in strength, she becomes more confident and the dynamic reverses itself, with Tuxedo Mask needing rescuing by the girls instead.",
+"One of the more interesting subtexts of the old anime is how often consumerism is used to take advantage of young women. The male villain Jadeite’s many schemes to harvest human energy typically involve feminine-coded consumer activities, including a jewelry store, a fitness center, and a talent search. As a friend put it to me, “Sailor Moon feels like it’s training girls to be constantly wary of situations where people are trying to sell you things.” When Usagi transforms into Sailor Moon to defeat Jadeite, she accompanies it with a speech about the evils of “taking advantage of young girls’ dreams” for selfish, harmful gains.",
+"The series also makes a point of showing a wide variety of personalities, interests and gender expressions among the Senshi. From bookish Ami (Mercury) to athletic Makoto (Jupiter) to artistic Michiru, there is a Senshi for every girl. Additionally, the series often makes a point of commenting on how the less traditionally feminine girls have trouble coping with gender roles, like how Makoto learned to cook because she was mocked for being a tomboy.",
+"Some say that Madoka is anti-feminist because it presents historical female accomplishment as a result of Kyubey’s actions therefore erasing them.  However I don’t read the show in that way at all.  Kyubey is sexist because it erases female accomplishment and like the patriarchy itself uses this to neg and negate the girls that it targets.  Breaking their spirits, reducing them to livestock, objects in its plans. \nThen we have the ending.  The structures of the old world torn down but despite Magoka’s efforts the new world isn't a utopia.  A constant reminder that every victory is just a progressive step one that has to be protected and effort has to be constantly applied to prevent backsliding and push us further to a truly equitable world.",
+"feminist studies, female characters are often regarded as reflections or ideal models of actual women. This interpretive link invites the argument that strong female characters in anime evidence women's empowerment in recent Japanese society. Susan Napier (2005, 33) argues, for example, that \"[p]opular youth-oriented anime series such as the 1980s Cutey Honey and the 1990s Sailor Moon show images of powerful young women (albeit highly sexualized in the case of Cutey Honey) that anticipate genuine, although small, changes in women's empowerment over the last two decades and certainly suggest alternatives to the notion of Japanese women as passive and domesticated.\"Kotani Mari (2006, 165-69) examines Revolutionary Girl Utena (1997) as a recent example of girls' anime that possibly subverts gender norms and conventional sexual politics for women. "
+];
+var posUrl = ["https://dspace.library.uvic.ca/bitstream/handle/1828/8496/russell_ndonna_MA_2017.pdf?sequence=1&isAllowed=y",
+"https://www.animenewsnetwork.com/interview/2014-05-16/exclusive/viz-media-charlene-ingram-and-josh-lopez-on-sailor-moon",
+"https://www.bitchmedia.org/post/the-feminism-of-sailor-moon",
+"https://reelrundown.com/animation/Is-Anime-Sexist-Against-Women-5-Current-or-Recent-Shows-Examined",
+"https://www.bitchmedia.org/post/the-feminism-of-sailor-moon",
+"https://www.bitchmedia.org/post/the-feminism-of-sailor-moon",
+"https://www.bitchmedia.org/post/the-feminism-of-sailor-moon",
+"https://www.bitchmedia.org/post/the-feminism-of-sailor-moon",
+"https://plus.google.com/+DavidFormosa/posts/ZbigqYHz8F6",
+"https://www.jstor.org/stable/43553398?Search=yes&resultItemClick=true&searchText=japanese&searchText=anime&searchText=gender&searchText=stereotype&searchUri=%2Faction%2FdoBasicSearch%3FQuery%3Djapanese%2Banime%2Bgender%2Bstereotype&refreqid=search%3Aa9d5e4bfaab8b349b660265207f53bae&seq=1#metadata_info_tab_contents"
+]
+
+var negative = [];
+var negQuote = ["While characters’ civilian personalities run the gamut of gender expression—tomboy Sailor Uranus often wears traditionally masculine clothes, for example—all the crime-fighting characters transform into impractical miniskirts and high heels when they take on the bad guys. It’s awesome to show that there’s power in the feminine, but not when it’s compulsory. Plus, it’s hard to argue that their costumes are purely about “empowerment” when those tiny miniskirts result in so many panty shots. ",
+"Haruka and Michiru, the series’ Sailor Uranus and Neptune, were a lesbian couple who helped girls around the world come to terms with their sexualities—if they were lucky to get a non-censored version of the series. Unfortunately, many international versions erased the girls’ sexualities, including the one released in the U.S. and Canada. They weren’t the only LGBTQ characters altered in the English dub, and those changes tarnished the series’ progressive legacy for many years.",
+"It could be accused of reinforcing gender stereotypes. Young girls are seen as easy prey for Kyubey because they're seen as emotional, irrational, and driven by silly, self-serving desires. If girls had smarter wishes, or asked more questions, the whole system would have collapsed long before modern times. This clearly shows girls as ridiculously trusting and naive. The magical girl concept seems like victim-blaming, even though the girls are manipulated into doing what Kyubey says by not being given adequate information on which to base their decision to transform into a magical girl. No one told them not to sign a contract without a lawyer present! ",
+"On the feminist side of things (yes), Kyubey is an agent to systems that are specifically designed to remove control from young women. He takes the “power” of these women, both physically and mentally, into his own control and has no qualms about disposing them for his own needs when they become useless to him. Often interpreted as analogous to the workings of female puberty, these young girls are turned into destructive transcendent forces because of the very processes Kyubey has deceptively put on them by taking advantage of emotional and mental weakness.",
+"“Witch” in itself is awfully appropriate wording in this case considering that, throughout history, women who grew old without husbands and decided to take agency over their own lives were seen by society as evil neglectors of the family. And yet, destructive monsters are exactly what these women become due to having that power manipulated away from them under the skeevy guise of “universal betterment”. An awfully powerful statement on what stagnant emotional disassociation does to humanity, in my eyes.",
+"Some scholars question whether the magical girls are really empowered. Napier repeatedly insists on the empowering effect of Cutey Honey's transformation, yet finally concludes that the eroticism and nudity emphasized in the transformation of Cutey Honey \"sends mixed messages\" (Napier 2005, 75). A more critical analysis is presented in Anne Allisons discussion of Sailor Moon , in which she defines the battle heroine as \"a self-indulgent pursuer of fantasies and dreams through consumption of merchandise\" (Allison 2006, 130). While male superheroes, like the Power Rangers, transform to change their body into a weapon to serve a higher goal, \"the process [of a girl hero's transformation] is more a 'makeover' than a power-up'\" (138). From a Japanese feminist perspective, Saito Minako (1998, 41) points out that children's television programs reinforce fixed gender roles functioning in actual society, thereby teaching girls to become a good daughter at home and a good OL2 at work. She argues that the female protagonist in the magical girl genre reconfirms the values of femininity, which teaches girls to envision marriage and domestic life as a desirable goal once they have passed the adolescent stage. Compared to female heroes who are cute juvenile girls, Saito continues, women in the enemy force, whether in magical girl programs or the Power Rangers series, are adult women wearing heavy makeup and obsessed with careerism: they are, simply put, the women who failed to be a wife or a mother. According to these arguments, the seemingly empowered girl heroes in anime covertly teach girls to pursue fashion, romance, and consumption until marriage and, once married, to stay at home as a good wife and mother",
+"I analyze how women’s voices are exploited in media to create various types of femininities, including the helpless femininity of kawaii. I then analyze women’s real words and discourse as they engage in discussions of femininity and women’s language, exploring how the gender stereotypes depicted in media are manifested, evoked, asserted, and rejected in the women’s speech.",
+"The connection between kawaii-ness and sexism might not be so apparent, but judging by Japan’s need to portray a strong, confident woman as innocent and naive, speaks volumes. The need for women to be submissive spills over into other realms of life.",
+"Hiroto Murasawa, an authority on beauty, warns that cute culture can instill “a mentalitiy the breeds non-assertion, individuals who choose to stand out get beaten down.”  With this mindset, more women are acting like children and engaging in childlike and childish behavior.  Women are tending to giggle more, speak in an intentionally high-pitched voice, wear childish clothing, throw temper tantrums or act purposely clueless.  A restaurant featuring maids that are eternally 17 years old perpetuates this youthful submissiveness.  The media encourages this cute display by broadcasting television shows hosted by teenagers, promoting pop stars that cry on command or idolizing actresses who ensure no one ever finds not cute"
+];
+var negUrl = ["https://www.bitchmedia.org/post/the-feminism-of-sailor-moon",
+"https://www.bitchmedia.org/post/the-feminism-of-sailor-moon",
+"https://reelrundown.com/animation/Is-Anime-Sexist-Against-Women-5-Current-or-Recent-Shows-Examined",
+"https://www.themarysue.com/psychopathy-feminism-and-narrative-agency-in-madoka-magica/",
+"https://www.themarysue.com/psychopathy-feminism-and-narrative-agency-in-madoka-magica/",
+"https://www.jstor.org/stable/43553398?Search=yes&resultItemClick=true&searchText=japanese&searchText=anime&searchText=gender&searchText=stereotype&searchUri=%2Faction%2FdoBasicSearch%3FQuery%3Djapanese%2Banime%2Bgender%2Bstereotype&refreqid=search%3Aa9d5e4bfaab8b349b660265207f53bae&seq=1#metadata_info_tab_contents",
+"https://csw.ucla.edu/2017/07/21/being-kawaii-in-japan/",
+"https://savvytokyo.com/sexism-culture-japans-obsession-kawaii/",
+"http://postbubbleculture.blogs.wm.edu/2012/11/18/cute-gets-creepy-examining-the-consequences-of-cute-culture/"
+];
+
+var pos = [];
+var neg = [];
+
+var num =0;
+var posnum =0;
+var negnum = 0;
+
+var buttons = [];
+var buttonsPos=[];
+
+var info;
+
+function preload(){
+	// get API use https instead http
+	hero1 = loadImage('asset/hero1.png');
+  	hero2 = loadImage('asset/hero2.png');
+  	hero3 = loadImage('asset/hero3.png');
+  	bad1 = loadImage('asset/bad1.png');
+  	bad2 = loadImage('asset/bad2.png');
+  	bad3 = loadImage('asset/bad3.png');
+
+  	//var url ='asset/animeQuote.json';
+  	// info = loadJSON('/Users/RianIshikawa/Desktop/CT_system/fianldemo/two/asset/animeQuote.json');
+  	//info = loadJSON('https://github.com/kyoheria/AnimeQuoteJason/blob/master/animeQuote.json');
+  	var xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function() {
+  	if (this.readyState == 4 && this.status == 200) {
+    	var myObj = JSON.parse(this.responseText);
+    	document.getElementById("demo").innerHTML = myObj.name;
+  	}
+};
+xmlhttp.open("GET", "asset/animeQuote.json", true);
+xmlhttp.send();
+}
+
+function setup() {
+	//canvas size
+	createCanvas(window.innerWidth, window.innerHeight);
+	//console.log(posQuote.length);
+	//values
+	//positive.push("")
+	// //for the demo adding word to the arraylist
+	// for(var i = 0; i<positive; i++){
+	// 	pos.push("positive");
+	// 	neg.push("negative");
+	// }
+
+}
+
+function showData(data){
+	//console.log(data);
+}
+
+function draw() {
+	background(255);
+	image(hero1,500,500);
+	//console.log("drawing");
+	//console.log(pos.length);
+	for(var i=0 ; i<neg.length; i++){
+		//console.log("display image");
+		//console.log(pos[i]);
+		var x = neg[i].getX();
+		var y = neg[i].getY();
+		var img = neg[i].getImage();
+		if(img ==1){
+			image(bad1,x,y);
+		} else if(img ==2){
+			image(bad2,x,y);
+		} else if(img ==3){
+			image(bad3,x,y);
+		}
+	}
+	for(var i=0 ; i<pos.length; i++){
+		//console.log("display image");
+		//console.log(pos[i]);
+		var x = pos[i].getX();
+		var y = pos[i].getY();
+		var img = pos[i].getImage();
+		if(img ==1){
+			image(hero1,x,y,216,300);
+		} else if(img ==2){
+			image(hero2,x,y,146,300);
+		} else if(img ==3){
+			image(hero3,x,y,168,300);
+		}
+		//console.log(pos[i].getQuote());
+		//text(pos[i].getQuote(), x,y);
+	}
+	if(frameCount%10 == 0){
+		var num = Math.floor(Math.random()*2);
+		if(num == 0){
+			pos.push(new hero());
+		}else{
+			neg.push(new bad());
+		}
+	}
+
+	for(var i=0 ; i<buttons.length; i++){
+		//console.log("display image");
+		//console.log(pos[i]);
+		var button = buttons[i];
+		var x = Math.floor(Math.random()*(width));;
+		var y = Math.floor(Math.random()*(height/2)+height/2);
+		button.position(buttonsPos[i].x,buttonsPos[i].y);
+		//console.log(buttonsPos[i]);
+  		button.size(200,300);
+		button.mousePressed(greet);
+	}
+	// fill('rgba(0,255,0, 0.25)');
+	// rect(0,0,width/2,height);
+	// fill('rgba(255,0,0, 0.25)');
+	// rect(width/2,0,width,height);
+	//console.log(buttons.length);
+}
+
+// function mouseClicked(){
+// 	var posneg = Math.floor(Math.random()*2);
+// 	//console.log("clicking");
+// 	//console.log(posneg);
+// 	if(mouseX<width/2){
+// 		pos.push(new hero());
+// 		console.log(pos);
+
+// 	}else{
+// 		neg.push(new bad());
+// 	}
+// }
+
+class hero{
+
+	constructor(){
+		var num = Math.floor(Math.random()*3)+1;
+		//var hero = "hero"+num;
+		this.image =num;
+		this.posX = Math.floor(Math.random()*(width));
+		this.posY = Math.floor(Math.random()*(height/2)+height/2);
+		var i = Math.floor(Math.random()*(posQuote.length));
+		//console.log(i);
+		this.quote = ""+posQuote[i];
+		this.url = posUrl[i];
+		var x = Math.floor(Math.random()*(width));;
+		var y = Math.floor(Math.random()*(height/2)+height/2);
+		buttons.push(createButton(this.quote));
+		buttonsPos.push(createVector(x,y));
+		// button.position(this.posX,this.posY);
+  //  		button.size(this.posX,this.posY);
+  //  		button.mousePressed(greet);
+	}
+	getImage(){
+		return this.image;
+	}
+	getX(){
+		return this.posX;
+	} 
+	getY(){
+		return this.posY;
+	} 
+	getQuote(){
+		return this.quote;
+	} 
+}
+
+class bad{
+
+	constructor(){
+		var num = Math.floor(Math.random()*3);
+		//var hero = "hero"+num;
+		this.image =num+1;
+		this.posX = Math.floor(Math.random()*(width));
+		this.posY = Math.floor(Math.random()*(height/2)-100);
+		var i = Math.floor(Math.random()*(posQuote.length));
+		//console.log(i);
+		this.quote = ""+negQuote[i];
+		this.url = negUrl[i];
+		var x = Math.floor(Math.random()*(width));;
+		var y = Math.floor(Math.random()*(height/2));
+		buttons.push(createButton(this.quote));
+		buttonsPos.push(createVector(x,y));
+	}
+	getImage(){
+		return this.image;
+	}
+	getX(){
+		return this.posX;
+	} 
+	getY(){
+		return this.posY;
+	} 
+}
+
+function greet() {
+    window.location.href = "<a href=\"http://www.google.com\" target=\"_blank\" rel=\"nofollow\">https://dspace.library.uvic.ca/bitstream/handle/1828/8496/russell_ndonna_MA_2017.pdf?sequence=1&isAllowed=y";
+}
